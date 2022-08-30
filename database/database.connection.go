4302383dcd0cea091d2a004616e5e4bcf0c45260
eniworoeva <eniworoeva@yahoo.com>
2022-08-30 11:53:46 +0100

    database setup

--- database/database.connection.go
+++ database/database.connection.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,4 +24,21 @@ func DBinstance() *mongo.Client {
 		log.Fatal(err)
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connect to MongoDB")
+	return client
+
+}
+
+var Client *mongo.Client = DBinstance()
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("book-DB").Collection(collectionName)
+	return collection
 }
