ec21b9d5e01555cb40a4d1007a41575bb64aeecd
eniworoeva <eniworoeva@yahoo.com>
2022-09-06 17:04:02 +0100

    routes

---
+++ routes/book.route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controllers "github.com/eniworoeva/books-CRUD-app/controller"
+)
+
+
+
+func BookRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("books/create", controllers.CreateBook())
+	//incomingRoutes.GET("books/:book_id", controllers.Getbook())
+	//incomingRoutes.PATCH("books/:book_id", controllers.Updatebook())
+	//incomingRoutes.DELETE("books/:book_id", controllers.DeleteBook())
+	//incomingRoutes.GET("books", controllers.GetAllBook())
+}
